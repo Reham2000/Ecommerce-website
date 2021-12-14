7b16822ca3ba8252545ff9854859147b0a3ee34c
amr-mohamed1 <amressa201311@gmail.com>
2021-12-14 21:33:45 +0200

    14/12

---
+++ layout/sidNav.php
@@ -0,0 +1,46 @@
+<div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="<?php echo "admin_dash.php";?>">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Content</div>
+                            
+                            <?php if($_SESSION['role'] == "1"){ ?>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts9" aria-expanded="false" aria-controls="collapseLayouts9">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Admins
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts9" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo "add_admin.php";?>">Add Admin</a>
+                                    <a class="nav-link" href="<?php echo "all_admins.php";?>">Edit / Delete Admin</a>
+                                </nav>
+                            </div>
+                            <?php  } ?>
+
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts5" aria-expanded="false" aria-controls="collapseLayouts5">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Patients
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div style="background-color: unset!important;" class="collapse" id="collapseLayouts5" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo "add_patient.php";?>">Add Patients</a>
+                                    <a class="nav-link" href="<?php echo "all_patients.php";?>">Edit / Delete Patients</a>
+                                </nav>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as: </div>
+                        Admin
+                    </div>
+                </nav>
+            </div>
+
